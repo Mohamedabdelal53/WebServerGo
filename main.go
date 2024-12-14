37583ccbef8465c51c064a38964a0a792a633eb1
Mohamedabdelal53 <mabdelal53@gmail.com>
2024-12-14 18:17:21 +0200

    had

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	godotenv.Load()
+
 	fmt.Println("Hello World")
+	PortString := os.Getenv("PORT")
+	if PortString == ""{
+		log.Fatal("Port Not Found")
+	}
+	fmt.Println("Port:",PortString)
 }
